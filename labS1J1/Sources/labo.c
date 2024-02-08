8eb8e2589b46b44263f684c48bebf7d191136b75
yonahwa <2330034@bart.ca>
2024-02-08 10:53:11 -0500

    modif pour changer de pc

--- labS1J1/Sources/labo.c
+++ labS1J1/Sources/labo.c
@@ -1,18 +1,15 @@
 #include"labo.h"
 #include <stdbool.h>
 
-typedef struct {
-	char characters;
-	int lenght;
-}String;
 
 char* sousTexte(char* texte, int from, int to) {
 	int i = 0;
-	char str[1403] = {0};
-	while (i + from <= to) {
+	static char str[512] = {0};
+	while (i + from < to) {
 		str[i] = texte[from + i];
 		i++;
-	} 
+	}
+	str[i] = '\0';
 	return str;
 };
 
@@ -26,8 +23,8 @@ void set_String(char* text, String* s) {
 
 int String_is_palindrome(String* s) {
 
-	for (int i = 0;i < Sizeof(s);i++) {
-		if (s[i] == s[Sizeof(s) - i]) {
+	for (int i = 0;i < sizeof(s);i++) {
+		if (s->characters[i] == s->characters[sizeof(s) - i]) {
 			printf("1");
 		}
 		else {
